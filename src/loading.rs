e59823ad768156048a38a8a97a8c17853f813b66
MateuszFido <mafido@ethz.ch>
2025-07-25 23:00:58 +0200

    feat: changed to accept command line args

--- src/loading.rs
+++ src/loading.rs
@@ -1,36 +1,21 @@
-use mzdata::prelude::SpectrumLike;
-use mzdata::spectrum::MultiLayerSpectrum;
+use mzdata::spectrum::{MultiLayerSpectrum, SpectrumLike};
 use mzdata::MzMLReader;
-use std::fs;
-use std::mem::size_of_val;
+use std::fs::File;
 use std::time::Instant;
 
-
-pub fn load_mzml() {
-
+pub fn load_mzml(path: &str) -> Vec<MultiLayerSpectrum> {
     let now = Instant::now();
-    let reader = MzMLReader::new(fs::File::open("tests/ANIL_1mM_1_neg.mzML").unwrap());
 
-    // automatically boxed before putting in a vector
-    let mut ms1_scans: Vec<MultiLayerSpectrum> = Vec::new(); 
+    let file = File::open(path).expect("Could not open file");
+    let reader = MzMLReader::new(file);
 
-    let mut ms2_scans: Vec<MultiLayerSpectrum> = Vec::new();
+    // Parallel processing of scans
+    let ms1_scans: Vec<MultiLayerSpectrum> = reader.filter(|scan| scan.ms_level() == 1).collect();
 
-    for spectrum in reader {
-        if spectrum.ms_level() == 1 {
-            ms1_scans.push(spectrum);
-        }
-        else if spectrum.ms_level() == 2 {
-            ms2_scans.push(spectrum);
-        }
-    }
-
-    println!("Found {} ms1 scans", ms1_scans.len());
-    println!("Found {} ms2 scans", ms2_scans.len());
-    let size_of_ms1 = size_of_val(&*ms1_scans) as f32 / 1024.0 / 1024.0;
-    let size_of_ms2 = size_of_val(&*ms2_scans) as f32 / 1024.0 / 1024.0;
-    println!("Size of MS1 scans in bytes: {size_of_ms1} MB");
-    println!("Size of MS2 scans in bytes: {size_of_ms2} MB");
-
-    println!("Time elapsed: {} ms", now.elapsed().as_millis());
+    println!(
+        "Loaded {} scans in {} seconds.",
+        ms1_scans.len(),
+        now.elapsed().as_secs_f32()
+    );
+    ms1_scans
 }
--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,21 @@ pub mod measurements;
 pub mod processing;
 
 use crate::loading::load_mzml;
-use crate::measurements::{LCMeasurement, MSMeasurement};
-use crate::processing::construct_xics;
+use std::env;
 
 fn main() {
-    println!("Running LCMSpector backend version {}", env!("CARGO_PKG_VERSION"));
-    
+    println!(
+        "Running LCMSpector backend version {}",
+        env!("CARGO_PKG_VERSION")
+    );
+    let args: Vec<String> = env::args().collect();
+
+    for (i, file_path) in args.iter().enumerate() {
+        if i == 0 {
+            continue; // Skip the first argument as it's the program name
+        }
+
+        println!("Reading from: {file_path}");
+        load_mzml(file_path);
+    }
 }
